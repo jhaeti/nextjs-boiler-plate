0fe52df8d205edc1e494773f7f4aa0a36c0b8355
TI JHAE <jhaeti@gmail.com>
2021-05-26 15:48:49 +0000

    Registration page Created

---
+++ frontend/pages/register.js
@@ -0,0 +1,12 @@
+import { Layout } from "../components/Layout";
+import Register from "../components/Register";
+
+const register = () => {
+  return (
+    <Layout>
+      <Register />
+    </Layout>
+  );
+};
+
+export default register;
