62de441a95cb57322c758f29a1a5685a9d33f299
TI JHAE <jhaeti@gmail.com>
2021-05-26 17:46:09 +0000

    Added routes for authorizing and authenticating user

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+const { setCookies } = require("./controllers/authMiddleware");
+
+const route = express.Router();
+
+// Register Route
+route.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json("Please enter all fields");
+  } else {
+    //   Check whether user already exist
+    User.findOne({ email }).then((user) => {
+      if (user) {
+        res.status(400).json("User already exist");
+      } else {
+        const newUser = new User({ name, email, password });
+        //   Hashing password before sending to mongoddb
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) {
+            res.json("Server Side Error.Please try again");
+          }
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            // Save user into DataBase
+            newUser.save().then((user) => {
+              jwt.sign(
+                { id: user.id },
+                process.env.JWT_SECRET_KEY,
+                (err, token) => {
+                  const { name, email, id } = user;
+                  if (err) throw err;
+
+                  // Saving token in coookies before sending data
+                  setCookies;
+                  res.json({ token, user: { name, email, id } });
+                }
+              );
+            });
+          });
+        });
+      }
+    });
+  }
+});
+
+// Login Route
+route.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json("Please enter all fields");
+  } else {
+    //   Check whether user already exist
+    User.findOne({ email }).then((user) => {
+      if (!user) {
+        res.status(400).json("User doesnot exist");
+      } else {
+        //   Compare plain text Password to Hash Password
+        bcrypt.compare(password, user.password).then((isMatch) => {
+          if (isMatch) {
+            jwt.sign(
+              { id: user.id },
+              process.env.JWT_SECRET_KEY,
+              (err, token) => {
+                const { name, email, id } = user;
+                if (err) throw err;
+
+                // Saving token in coookies before sending data
+                setCookies;
+                res.json({ token, user: { name, email, id } });
+              }
+            );
+          } else {
+            res.status(400).json("Invalid credentials");
+          }
+        });
+      }
+    });
+  }
+});
+
+module.exports = route;
---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+var cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Add middleware
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json({ extended: false }));
+
+// Connect To route below
+app.use("/api/items", require("./routes/itemRoute"));
+app.use("/api/users", require("./routes/userRoute"));
+app.use("/api/auth", require("./routes/authRoute"));
+
+// Getting mongo uri from the keys folder
+const url =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI
+    : process.env.MONGO_DEV_URI;
+
+// Connect to mongodb using mongoose
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+mongoose.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("MongoDb connected...")
+);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`Server started on port ${port}`));
