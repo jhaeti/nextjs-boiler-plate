63a1c5f03e352d1ffe94bb3d3f9fc2d981b75961
TI JHAE <jhaeti@gmail.com>
2021-06-14 01:32:57 +0000

    Cleaning up code

--- backend/routes/controllers/authMiddleware.js
+++ backend/routes/controllers/authMiddleware.js
@@ -1,22 +1,4 @@
 const jwt = require("jsonwebtoken");
-// module.exports = (req, res, next) => {
-//   // const token = req.header("x-auth-token");
-//   const token = req.cookies[process.env.AUTH_COOKIE_NAME];
-
-//   if (!token) {
-//     res.status(401).json("No token, Authorization denied");
-//   } else {
-//     try {
-//       // Verify token
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-//       req.user = decoded;
-//       req.token = token;
-//       next();
-//     } catch (e) {
-//       res.status(400).json("Token is not valid");
-//     }
-//   }
-// };
 
 module.exports = {
   // Middleware for Authorizing old users
@@ -24,6 +6,7 @@ module.exports = {
     // get token from cookies
     const token = req.cookies[process.env.AUTH_COOKIE_NAME];
 
+    // Return 401 statusCode if there is no token in cookies
     if (!token) {
       res.status(401).json("No token, Authorization denied");
     } else {
