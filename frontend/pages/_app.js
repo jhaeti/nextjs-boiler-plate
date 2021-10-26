6f834a31066d6cd46337fd04c6865d8b61d93649
unknown <jhaeti@gmail.com>
2021-10-26 07:19:06 +0000

    Updated _app.js to use scss folder for css

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,5 @@
 import { wrapper } from "../redux/store";
-import "../public/css/style.scss";
+import "../public/scss/style.scss";
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default wrapper.withRedux(MyApp);
