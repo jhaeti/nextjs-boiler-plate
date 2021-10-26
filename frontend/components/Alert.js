3cdc00a9748ee37ae17f231d1badbfae75417f0e
unknown <jhaeti@gmail.com>
2021-10-26 07:25:41 +0000

    Fixed calling setTimeout bug, implemented the BEM and removed component styling

--- frontend/components/Alert.js
+++ frontend/components/Alert.js
@@ -2,57 +2,34 @@ import { connect } from "react-redux";
 import { clearMsg } from "../redux/actions/userAction";
 
 const Alert = ({ msg, clearMsg }) => {
-  return (
-    msg && (
-      <div className="container">
-        <div className="msg">
-          {msg}
-
-          <div
-            onClick={() => {
-              clearMsg();
-            }}
-            className="x"
-          >
-            x
-          </div>
-        </div>
-
-        <div className="bug">{setTimeout(() => clearMsg(), 3000)}</div>
-        <style jsx>{`
-          .container {
-            justify-content: flex-end;
-          }
-          .msg {
-            background: hsl(131, 60%, 81%);
-            padding: 1rem 0rem 1rem 1.5rem;
-
-            margin-top: 1rem;
-            color: hsl(131, 50%, 21%);
-            position: absolute;
-            z-index: 2;
-            cursor: pointer;
-          }
-          .x {
-            display: inline;
-            padding: 1rem 1.5rem;
-            color: #f00;
-            opacity: 0.7;
-            cursor: pointer;
-          }
-          .bug {
-            display: none;
-          }
-        `}</style>
-      </div>
-    )
-  );
+    return (
+        msg &&
+        //  CLears Message after 3s.
+        setTimeout(() => {
+            clearMsg();
+        }, 3000) && (
+            <div className="alert row row--end">
+                <div className="alert__msg mr--5">
+                    {msg}
+                    <div
+                        onClick={() => {
+                            clearMsg();
+                        }}
+                        className="alert__close-btn"
+                    >
+                        x
+                    </div>
+                </div>
+                <style jsx>{``}</style>
+            </div>
+        )
+    );
 };
 
 const mapStateToProps = (state) => {
-  return {
-    msg: state.auth.msg,
-  };
+    return {
+        msg: state.auth.msg,
+    };
 };
 
 export default connect(mapStateToProps, { clearMsg })(Alert);
