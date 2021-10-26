5cb347c0e14ffb9a78880e23e84a50b44b6c0458
unknown <jhaeti@gmail.com>
2021-10-26 07:22:37 +0000

    Updated to use the BEM methodology and removed component styling

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -4,23 +4,32 @@ import Alert from "./Alert";
 import Head from "next/head";
 
 export const Layout = ({ children, title, description }) => {
-  return (
-    <div>
-      <Head>
-        <title>{title ? title : ""}</title>
-        <meta charSet="UTF-8" />
-        <meta name="description" content={description ? description : ""} />
-        <meta
-          name="keywords"
-          content="Nextjs, React, Node, Boilerplate, Redux, Mongodb"
-        />
-        <meta name="author" content="Ti Jhae" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <Navbar />
-      <Alert />
-      {children}
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Head>
+                <title>{title ? title : ""}</title>
+                <meta charSet="UTF-8" />
+                <meta
+                    name="description"
+                    content={description ? description : ""}
+                />
+                <meta
+                    name="keywords"
+                    content="Nextjs, React, Node, Boilerplate, Redux, Mongodb"
+                />
+                <meta name="author" content="Ti Jhae" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <Navbar />
+            <div className="col-fill-between">
+                <Alert />
+                {children}
+            </div>
+
+            <Footer />
+        </div>
+    );
 };
--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,126 +1,79 @@
 import Link from "next/link";
-import Menutoggler from "./Menutoggler";
-import { Component } from "react";
+import { useRouter } from "next/router";
+// import Menutoggler from "./Menutoggler";
 import { connect } from "react-redux";
 import { logout } from "../redux/actions/userAction";
 
 const Navbar = ({ isAuthenticated, logout }) => {
-  return (
-    <div className="navbar">
-      <div className="container">
-        <div className="navbranding">
-          <Link href="/">
-            <a>LOGO</a>
-          </Link>
+    const isCurrentPage = (path) => {
+        const router = useRouter();
+        return router.pathname === path ? "active nav__link" : "nav__link";
+    };
+    return (
+        <div className="nav">
+            <div className="container">
+                <div className="nav__box">
+                    <div className="nav__branding">
+                        <Link href="/">
+                            <a className="nav__logo">LOGO</a>
+                        </Link>
+                    </div>
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <Link href="/">
+                                <a className={isCurrentPage("/")}>Home</a>
+                            </Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link href="/about">
+                                <a className={isCurrentPage("/about")}>About</a>
+                            </Link>
+                        </li>
+                        {!isAuthenticated ? (
+                            <>
+                                <li className="nav__item">
+                                    <Link href="/register">
+                                        <a
+                                            className={isCurrentPage(
+                                                "/register"
+                                            )}
+                                        >
+                                            Register
+                                        </a>
+                                    </Link>
+                                </li>
+                                <li className="nav__item">
+                                    <Link href="/login">
+                                        <a className={isCurrentPage("/login")}>
+                                            Login
+                                        </a>
+                                    </Link>
+                                </li>
+                            </>
+                        ) : (
+                            <li className="nav__item">
+                                <Link href="/login">
+                                    <a
+                                        className={isCurrentPage("/login")}
+                                        onClick={() => {
+                                            logout();
+                                        }}
+                                    >
+                                        Logout
+                                    </a>
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
         </div>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-          {!isAuthenticated ? (
-            <>
-              <li>
-                <Link href="/register">
-                  <a>Register</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/login">
-                  <a>Login</a>
-                </Link>
-              </li>
-            </>
-          ) : (
-            <li>
-              <Link href="/login">
-                <a
-                  onClick={() => {
-                    logout();
-                  }}
-                >
-                  Logout
-                </a>
-              </Link>
-            </li>
-          )}
-        </ul>
-        <div className="toggle-btn">
-          <Menutoggler />
-        </div>
-      </div>
-
-      <style jsx>{`
-        .navbar {
-          height: 60px;
-          width: 100%;
-          background: #333;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          color: #fff;
-        }
-
-        .container {
-          width: 1366px;
-          margin: 0 auto;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          height: 100%;
-        }
-
-        ul {
-          display: flex;
-        }
-
-        ul li {
-          margin: 0 10px;
-          list-style: none;
-        }
-
-        a {
-          text-decoration: none;
-          color: #fff;
-        }
-
-        .toggle-btn {
-          display: none;
-        }
-
-        // LapTops Screens
-        @media only screen and (max-width: 1440px) {
-          .container {
-            width: 80%;
-          }
-        }
-
-        // Mobile version
-        @media only screen and (max-width: 767px) {
-          .container {
-            width: 90%;
-          }
-          .toggle-btn {
-            display: block;
-          }
-          ul {
-            display: none;
-          }
-        }
-      `}</style>
-    </div>
-  );
+    );
 };
 
 const mapStateToProps = (state) => {
-  return {
-    isAuthenticated: state.auth.isAuthenticated,
-  };
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
+    };
 };
 export default connect(mapStateToProps, { logout })(Navbar);
