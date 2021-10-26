db8333f2d96f82a3eca9aa63b789ad996e4e4d15
unknown <jhaeti@gmail.com>
2021-10-26 07:39:53 +0000

    Updated to use BEM and remove component styling

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -4,96 +4,83 @@ import { login } from "../redux/actions/userAction";
 import Link from "next/link";
 
 const Login = ({ login }) => {
-  const [state, setState] = useState({ email: "", password: "" });
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value });
-  };
+    const [state, setState] = useState({ email: "", password: "" });
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value });
+    };
 
-  // Attempting to Login
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    login(state);
-  };
-  return (
-    <div className="page">
-      <div className="container">
-        <div className="left">
-          <div className="title-lg">Login</div>
-          <div className="eula">
-            By logging in you agree to the ridiculously long terms that you
-            didn't bother to read
-          </div>
+    // Attempting to Login
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        login(state);
+    };
+    return (
+        <div className="col col--center col-fill-between">
+            <div className="container">
+                <div className="row row--center">
+                    <div className="login-box absolute-center">
+                        <div className="login-box__left pl--4 pr--4 pt--8 pb--8">
+                            <h2 className="heading-primary heading-primary--bold mb--4">
+                                Login
+                            </h2>
+                            <p className="paragraph paragraph--sm">
+                                By logging in you agree to the ridiculously long
+                                terms that you didn't bother to read
+                            </p>
+                        </div>
+                        <div className="login-box__right pl--4 pr--4 pt--3 pb--3">
+                            <form
+                                onSubmit={handleSubmit}
+                                className="login-box__form"
+                            >
+                                <label
+                                    htmlFor="email"
+                                    className="login-box__label"
+                                >
+                                    Email
+                                </label>
+                                <input
+                                    className="login-box__input"
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    onChange={handleChange}
+                                    value={state.email}
+                                    required
+                                />
+                                <label
+                                    htmlFor="password"
+                                    className="login-box__label mt--2"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    className="login-box__input"
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    onChange={handleChange}
+                                    value={state.password}
+                                    required
+                                />
+                                <Link href="/register">
+                                    <a className="login-box__link mt--2">
+                                        Create an account
+                                    </a>
+                                </Link>
+                                <input
+                                    className="login-box__submit mt--3"
+                                    type="submit"
+                                    id="submit"
+                                    value="Submit"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="right">
-          <form onSubmit={handleSubmit} className="form">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChange}
-              value={state.email}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={handleChange}
-              value={state.password}
-            />
-            <Link href="/register">
-              <a>Create an account</a>
-            </Link>
-            <input type="submit" id="submit" value="Submit" />
-          </form>
-        </div>
-      </div>
-      <style jsx>{`
-        .container {
-          width: 640px;
-          height: 100%;
-          align-items: center;
-        }
-        input[type="submit"] {
-          cursor: pointer;
-        }
-        input[type="email"],
-        input[type="password"] {
-          border-bottom: 1px solid #ddd;
-        }
-        @media (max-width: 767px) {
-          .container {
-            width: 320px;
-            height: 630px;
-            flex-direction: column;
-            justify-content: center;
-          }
-          .left {
-            height: 100%;
-            width: calc(100% - 40px);
-            max-height: 250px;
-            position: relative;
-            top: 10px;
-          }
-          .right {
-            flex-shrink: 0;
-            height: 100%;
-            width: 100%;
-            max-height: 300px;
-            position: relative;
-            top: -10px;
-          }
-        }
-        a {
-          color: #fff;
-          display: block;
-          margin-top: 20px;
-          text-align: center;
-        }
-      `}</style>
-    </div>
-  );
+    );
 };
 
 export default connect(null, { login })(Login);
--- frontend/components/Register.js
+++ frontend/components/Register.js
@@ -4,101 +4,98 @@ import { register } from "../redux/actions/userAction";
 import Link from "next/link";
 
 const Register = ({ register }) => {
-  const [state, setState] = useState({ name: "", email: "", password: "" });
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value });
-  };
+    const [state, setState] = useState({ name: "", email: "", password: "" });
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value });
+    };
 
-  //   Attempting to Register a User
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    register(state);
-  };
-  return (
-    <div className="page">
-      <div className="container">
-        <div className="left">
-          <div className="title-lg">Register</div>
-          <div className="eula">
-            By registering in you gain access to adding items and deleting items
-            of your choice
-          </div>
+    //   Attempting to Register a User
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        register(state);
+    };
+    return (
+        <div className="col col--center col-fill-between">
+            <div className="container">
+                <div className="row row--center">
+                    <div className="register-box absolute-center">
+                        <div className="register-box__left pl--4 pr--4 pt--8 pb--8">
+                            <h2 className="heading-primary heading-primary--bold mb--4">
+                                Register
+                            </h2>
+                            <p className="paragraph paragraph--sm">
+                                By registering in you gain access to adding
+                                items and deleting items of your choice
+                            </p>
+                        </div>
+                        <div className="register-box__right pl--4 pr--4 pt--3 pb--3">
+                            <form
+                                onSubmit={handleSubmit}
+                                className="register-box__form"
+                            >
+                                <label
+                                    className="register-box__label"
+                                    htmlFor="name"
+                                >
+                                    Name
+                                </label>
+                                <input
+                                    className="register-box__input"
+                                    type="name"
+                                    id="name"
+                                    name="name"
+                                    onChange={handleChange}
+                                    value={state.name}
+                                    required
+                                />
+                                <label
+                                    className="register-box__label mt--2"
+                                    htmlFor="email"
+                                >
+                                    Email
+                                </label>
+                                <input
+                                    className="register-box__input"
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    onChange={handleChange}
+                                    value={state.email}
+                                    required
+                                />
+                                <label
+                                    className="register-box__label mt--2"
+                                    htmlFor="password"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    className="register-box__input"
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    onChange={handleChange}
+                                    value={state.password}
+                                    required
+                                />
+                                <Link href="/login">
+                                    <a className="register-box__link mt--2">
+                                        Already have an account
+                                    </a>
+                                </Link>
+                                <input
+                                    className="register-box__submit mt--3"
+                                    type="submit"
+                                    id="submit"
+                                    value="Submit"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="right">
-          <form onSubmit={handleSubmit} className="form">
-            <label htmlFor="name">Name</label>
-            <input
-              type="name"
-              id="name"
-              name="name"
-              onChange={handleChange}
-              value={state.name}
-            />
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChange}
-              value={state.email}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={handleChange}
-              value={state.password}
-            />
-            <Link href="/login">
-              <a>Already have an account</a>
-            </Link>
-            <input type="submit" id="submit" value="Submit" />
-          </form>
-        </div>
-      </div>
-      <style jsx>{`
-        .container {
-          width: 640px;
-          height: 100%;
-          align-items: center;
-        }
-        input[type="email"],
-        input[type="name"],
-        input[type="password"] {
-          border-bottom: 1px solid #ddd;
-        }
-        @media (max-width: 767px) {
-          .container {
-            width: 320px;
-            height: 630px;
-            flex-direction: column;
-            justify-content: center;
-          }
-          .left {
-            height: 100%;
-            width: calc(100% - 40px);
-            max-height: 250px;
-            position: relative;
-            top: 10px;
-          }
-          .right {
-            flex-shrink: 0;
-            height: 100%;
-            width: 100%;
-            position: relative;
-            top: -10px;
-          }
-        }
-        a {
-          color: #fff;
-          display: block;
-          margin-top: 20px;
-          text-align: center;
-        }
-      `}</style>
-    </div>
-  );
+    );
 };
 
 export default connect(null, { register })(Register);
