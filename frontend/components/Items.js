16f24dd3b7934bb924fc882aa212ee45008ef662
unknown <jhaeti@gmail.com>
2021-10-29 21:15:50 +0000

    No changes made

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -2,13 +2,13 @@ import { connect } from "react-redux";
 import Item from "./Item";
 
 const Items = ({ items }) => {
-  return items.map((item) => <Item key={item._id} item={item} />);
+    return items.map((item) => <Item key={item._id} item={item} />);
 };
 
 const mapStateToProps = (state) => {
-  return {
-    items: state.item.items,
-  };
+    return {
+        items: state.item.items,
+    };
 };
 
 export default connect(mapStateToProps, null)(Items);
