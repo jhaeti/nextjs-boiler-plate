6397d04da74fc7e881c8c483047fcf2f4d219abe
unknown <jhaeti@gmail.com>
2021-10-31 08:41:46 +0000

    Updated server to load express server from app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,4 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-
-require("dotenv").config();
-require("./db/mongoose");
-// require("./app");
-
-const itemRouter = require("./routes/item");
-const userRouter = require("./routes/user");
-
-const app = express();
-
-// Add middleware
-app.use(cookieParser());
-app.use(
-    cors({
-        origin: [process.env.CLIENT_URL, "http://localhost:3000"],
-        credentials: true,
-    })
-);
-app.use(express.json({ extended: false }));
-
-// Using routers
-app.use(itemRouter);
-app.use(userRouter);
+const app = require("./app");
 
 const port = process.env.PORT;
 
